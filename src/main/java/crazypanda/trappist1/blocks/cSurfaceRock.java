7a006d6323ef24f55ff71b252e7524e3bf5e2e03
CrazyPanda <crazypanda741@gmail.com>
2019-08-05 14:25:18 -0700

    Added 3 blocks
    -Trappist-1c Surface Rock
    -Trappist-1c Sub-Surface Rock
    -Trappist-1c Rock

---
+++ src/main/java/crazypanda/trappist1/blocks/cSurfaceRock.java
@@ -0,0 +1,20 @@
+package crazypanda.trappist1.blocks;
+
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+
+public class cSurfaceRock extends BlockBase
+{
+
+	public cSurfaceRock(String name, Material material) 
+	{
+		
+		super(name, material);
+		
+		setSoundType(SoundType.STONE);
+		setHardness(2.0F);
+		setResistance(10.0F);
+		setHarvestLevel("pickaxe", 0);
+	}
+
+}
--- src/main/java/crazypanda/trappist1/init/ModBlocks.java
+++ src/main/java/crazypanda/trappist1/init/ModBlocks.java
@@ -6,6 +6,9 @@ import java.util.List;
 import crazypanda.trappist1.blocks.bRock;
 import crazypanda.trappist1.blocks.bSubRock;
 import crazypanda.trappist1.blocks.bSurfaceRock;
+import crazypanda.trappist1.blocks.cRock;
+import crazypanda.trappist1.blocks.cSubRock;
+import crazypanda.trappist1.blocks.cSurfaceRock;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
@@ -16,4 +19,8 @@ public class ModBlocks
 	public static final Block B_SURFACEROCK = new bSurfaceRock("b_surfacerock", Material.ROCK);
 	public static final Block B_SUBROCK = new bSubRock("b_subrock", Material.ROCK);
 	public static final Block B_ROCK = new bRock("b_rock", Material.ROCK);
+	
+	public static final Block C_SURFACEROCK = new cSurfaceRock("c_surfacerock", Material.ROCK);
+	public static final Block C_SUBROCK = new cSubRock("c_subrock", Material.ROCK);
+	public static final Block C_ROCK = new cRock("c_rock", Material.ROCK);
 }
