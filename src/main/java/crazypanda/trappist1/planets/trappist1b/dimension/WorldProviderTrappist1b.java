03cc7b0e92ea02aff6a1eecf34554f933590ac1b
CrazyPanda <crazypanda741@gmail.com>
2019-08-28 08:01:33 -0700

    Changed WorldProvider extension

--- src/main/java/crazypanda/trappist1/planets/trappist1b/dimension/WorldProviderTrappist1b.java
+++ src/main/java/crazypanda/trappist1/planets/trappist1b/dimension/WorldProviderTrappist1b.java
@@ -3,15 +3,17 @@ package crazypanda.trappist1.planets.trappist1b.dimension;
 import java.util.Arrays;
 import java.util.List;
 
-import crazypanda.trappist1.core.WorldProviderBase;
 import crazypanda.trappist1.init.ModBlocks;
 import crazypanda.trappist1.planets.Trappist1_Planets;
 import crazypanda.trappist1.planets.trappist1b.world.gen.BiomeProviderTrappist1b;
 import crazypanda.trappist1.planets.trappist1b.world.gen.ChunkGeneratorTrappist1b;
 import crazypanda.trappist1.util.Trappist1Dimensions;
 import micdoodle8.mods.galacticraft.api.galaxies.CelestialBody;
+import micdoodle8.mods.galacticraft.api.prefab.world.gen.WorldProviderSpace;
 import micdoodle8.mods.galacticraft.api.vector.Vector3;
+import micdoodle8.mods.galacticraft.api.world.ISolarLevel;
 import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.DimensionType;
@@ -20,7 +22,7 @@ import net.minecraft.world.gen.IChunkGenerator;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-public class WorldProviderTrappist1b extends WorldProviderBase
+public class WorldProviderTrappist1b extends WorldProviderSpace implements ISolarLevel
 {
     @Override
     public Vector3 getFogColor()
@@ -99,16 +101,11 @@ public class WorldProviderTrappist1b extends WorldProviderBase
         }
     }
 
-    @Override
-    protected void renderSky()
-    {
-        
-    }
+   
 
     @Override
     public void init()
     {
-        super.init();
         this.biomeProvider = new BiomeProviderTrappist1b();
     }
 
@@ -140,5 +137,29 @@ public class WorldProviderTrappist1b extends WorldProviderBase
 	public boolean canSpaceshipTierPass(int tier) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public double getFuelUsageMultiplier() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ResourceLocation getDungeonChestType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean hasSunset() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Class<? extends IChunkGenerator> getChunkProviderClass() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
